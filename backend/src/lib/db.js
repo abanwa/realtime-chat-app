d9c5212d5b820225e7c75eb531070238e37f1e7e
abanwa <abanwachinaza@gmail.com>
2025-05-09 10:56:04 +0100

    RealTime Chat App Built

---
+++ backend/src/lib/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    // const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      // dbName: "RealTime-Chat-App", // Explicitly set the DB name (redundant if URI includes it)
+      connectTimeoutMS: 30000,     // 30-second connection timeout
+      socketTimeoutMS: 30000,      // 30-second socket timeout
+      // tls: true,                   // Force TLS/SSL (required for Atlas)
+    });
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log(`Using Database: ${mongoose.connection.db.databaseName}`); // Verify DB name
+  } catch (error) {
+    console.log("MongoDB connection error:", error);
+  }
+};
